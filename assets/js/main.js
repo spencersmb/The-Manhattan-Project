2d71955ed159fb0afb59c07f68d659506f7d3dad
spencersmb <spencer.bigum@gmail.com>
2014-11-21 19:30:12 -0500

    mac book
    changed min.’s

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,28 @@ jQuery(document).ready(function($){
 
 	var desktopLarge = 1170;
 
-	var windowWidth = $(window).width();
-	console.log("on load " + windowWidth);
+	function windowWidth(){
+
+		var windowWidth = $(window).width(),
+			resizeWidth;
+
+		$(window).resize(function() {
+			resizeWidth = $(window).width();
+		//console.log("resize" + wi);
+		});
+
+		if( resizeWidth || windowWidth >= desktopLarge){
+			console.log('desktop Large');
+		} else {
+			console.log('desktop not Large');
+		}
+	}
+	
+	//console.log("on load " + windowWidth);
 
 	var innerWidth = $(window).innerWidth();
 	console.log("inner width2 " + innerWidth);
-
-	$(window).resize(function() {
-		var wi = $(window).width();
-		console.log("resize" + wi);
-	});
+	var desktop = windowWidth();
+	console.log(desktop);
+	
 });
