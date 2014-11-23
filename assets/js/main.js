334498693c7644beb5f3c0d72f876915bb48d2d9
spencersmb <spencer.bigum@gmail.com>
2014-11-23 02:53:55 -0500

    11_23
    update to styles?

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,43 @@
 jQuery(document).ready(function($){
 
-	var desktopLarge = 1170;
+	var desktopLarge = 1170,
+	resizeWidth;
 
-	function windowWidth(){
+	$(window).on('resize', function(){
+		(!window.requestAnimationFrame) ? setTimeout(checkwindowWidth, 300) : window.requestAnimationFrame(checkwindowWidth);
+	});
 
-		var windowWidth = $(window).width(),
-			resizeWidth;
+	var windowWidth = $(window).width();
 
-		$(window).resize(function() {
-			resizeWidth = $(window).width();
-		//console.log("resize" + wi);
-		});
+	function checkwindowWidth(){
 
-		if( resizeWidth || windowWidth >= desktopLarge){
-			console.log('desktop Large');
-		} else {
-			console.log('desktop not Large');
-		}
-	}
+		
+			var width = $(window).width();
+			//console.log("width " + width);
+		// $(window).resize(function() {
+		// 	resizeWidth = $(window).width();
+		// //console.log("resize" + wi);
+		// });
+
+		// if( resizeWidth || windowWidth >= desktopLarge){
+		// 	console.log('desktop Large');
+		// } else {
+		// 	console.log('desktop small');
+		// }
+	};
 	
 	//console.log("on load " + windowWidth);
 
-	var innerWidth = $(window).innerWidth();
-	console.log("inner width2 " + innerWidth);
-	var desktop = windowWidth();
-	console.log(desktop);
+	// var innerWidth = $(window).innerWidth();
+	// console.log("inner width2 " + innerWidth);
+	var desktop = checkwindowWidth();
+	console.log("window width" + windowWidth);
+
+	console.log("width" + desktop);
+
+		$(window).resize(function() {
+			var wi = $(window).width();
+		console.log("resize" + wi);
+	});
 	
 });
