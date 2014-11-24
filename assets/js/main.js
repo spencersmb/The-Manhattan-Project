88250a8925c767180d717898d2ab008b25710798
spencersmb <spencer.bigum@gmail.com>
2014-11-24 18:57:11 -0500

    js update
    more js functionality added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,43 @@
 jQuery(document).ready(function($){
 
+
+//**Check offset of element
+//--------------------------------------
+	var h1Offset = $('h1').offset().top;
+	//console.log('offset '+h1Offset);
+
+	var h1height = $('h1').height();
+	//console.log('height '+h1height);
+
+	var totalh1 = h1height + h1Offset;
+	//console.log('total '+totalh1);
+
+	var h1parse = parseInt($('h1').css('margin-Top').replace('px', ''));
+	//just add + px to add px to the end of the string
+	//console.log('h1 parse '+h1parse);
+	
+//**Check where browser vertical distance
+//--------------------------------------
+
+	windowScrollTop();
+
+	function windowScrollTop() {
+
+		var windowTop = $(window).scrollTop();
+
+		$(window).on('scroll', function(){
+			var scrollTop = $(window).scrollTop();
+			//console.log(scrollTop);
+	});
+	//console.log(windowTop);
+	};
+
+
+
+
+//**Check browser window size & remove nav
+//--------------------------------------
+	var nav = $('.nav');
 	var desktopLarge = 1170,
 		resizedWidth;
 
@@ -30,13 +68,27 @@ jQuery(document).ready(function($){
 
 		if ( desktop ) {
         	console.log('desktop');
-			// navigation.detach();
-			// navigation.insertBefore('.cd-header-buttons');
+			nav.detach();
+			nav.insertBefore('#logo');
 		} else {
 			console.log('mobile');
-			// navigation.detach();
-			// navigation.insertAfter('.cd-main-content');
+			nav.detach();
+			nav.insertAfter('.container');
 		}
-	}
+		}
 	
-});
+
+	//smooth scrolling when clicking on the secondary navigation items
+	nav.find('li.go-back a').on('click',function(event){
+		event.preventDefault();
+		var target = $(this.hash);
+		$('body,html').animate({
+			'scrollTop': target.offset().top - h1height
+		}, 400
+		);
+	});
+
+});//end jquery
+
+
+	
